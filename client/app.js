215f5cd9d1269a7467c666cee38a9b6c444afb24
rfprod <zoidenmacht@zoho.com>
2017-04-06 09:51:26 -0700

    (fix) bundled app works; fixed DI; added dummy.js for lazy loading example; updated .gitignore

--- client/app.js
+++ client/app.js
@@ -31,133 +31,134 @@ var app = angular.module('appCore', [
 	'appCore.contentEditable',
 	'appCore.httpServices',
 	'appCore.userService'
-]).
-config(['$stateProvider', '$urlRouterProvider', 'usSpinnerConfigProvider', function($stateProvider, $urlRouterProvider, usSpinnerConfigProvider) {
+])
+.config(['$stateProvider', '$urlRouterProvider', 'usSpinnerConfigProvider',
+	function($stateProvider, $urlRouterProvider, usSpinnerConfigProvider) {
 
-	$urlRouterProvider.otherwise('/app/sign-in');
+		$urlRouterProvider.otherwise('/app/sign-in');
 
-	$stateProvider
-		.state('app', {
-			url: '/app',
-			abstract: true,
-			templateUrl: 'views/common/layout.html'
-		})
+		$stateProvider
+			.state('app', {
+				url: '/app',
+				abstract: true,
+				templateUrl: 'views/common/layout.html'
+			})
 
-		.state('app.sign-in', {
-			url: '/sign-in',
-			templateUrl: 'views/sign-in/sign-in.html',
-			controller: 'SignInController',
-			data: {
-				requireAuth: false,
-				pageTitle: 'Page.Title.Signin',
-				pageLead: 'Page.Brief.Signin'
-			}
-		})
-
-		.state('app.profile', {
-			url: '/profile',
-			templateUrl: 'views/profile/profile.html',
-			controller: 'ProfileController',
-			data: {
-				requireAuth: true,
-				pageTitle: 'Page.Title.Profile',
-				pageLead: 'Page.Brief.Profile'
-			}
-		})
-
-		.state('app.user', {
-			url: '/user',
-			abstract: true,
-			template: '<ui-view/>'
-		})
-			.state('app.user.dashboard', {
-				url: '/dashboard',
+			.state('app.sign-in', {
+				url: '/sign-in',
+				templateUrl: 'views/sign-in/sign-in.html',
+				controller: 'SignInController',
 				data: {
-					pageTitle: 'Page.Title.Dashboard',
-					pageLead: 'Page.Brief.Dashboard'
-				},
-				views:{
-					'':{
-						templateUrl: 'views/user-panel/user-panel.html',
-						controller: 'UserPanelController',
-					},
-					'analytics@app.user.dashboard':{
-						templateUrl: 'views/analytics/analytics.html',
-						controller: 'AnalyticsController'
-					}
-				},
-				resolve: {
-					loadPlugin: function($ocLazyLoad) {
-						return $ocLazyLoad.load([
-						]);
-					}
+					requireAuth: false,
+					pageTitle: 'Page.Title.Signin',
+					pageLead: 'Page.Brief.Signin'
 				}
 			})
 
-		.state('app.admin', {
-			url: '/admin',
-			abstract: true,
-			template: '<ui-view/>'
-		})
-			.state('app.admin.dashboard', {
-				url: '/dashboard',
+			.state('app.profile', {
+				url: '/profile',
+				templateUrl: 'views/profile/profile.html',
+				controller: 'ProfileController',
 				data: {
-					pageTitle: 'Page.Title.Dashboard',
-					pageLead: 'Page.Brief.Dashboard'
-				},
-				views:{
-					'':{
-						templateUrl: 'views/admin-panel/admin-panel.html',
-						controller: 'AdminPanelController',
-					},
-					'analytics@app.admin.dashboard':{
-						templateUrl: 'views/analytics/analytics.html',
-						controller: 'AnalyticsController'
-					},
-					'diag@app.admin.dashboard':{
-						templateUrl: 'views/diag/diag.html',
-						controller: 'DiagController'
-					}
-				},
-				resolve: {
-					loadPlugin: function($ocLazyLoad) {
-						return $ocLazyLoad.load([
-						]);
-					}
+					requireAuth: true,
+					pageTitle: 'Page.Title.Profile',
+					pageLead: 'Page.Brief.Profile'
 				}
 			})
-	;
 
-	usSpinnerConfigProvider.setDefaults({
-		lines: 13, // The number of lines to draw
-		length: 28, // The length of each line
-		width: 14, // The line thickness
-		radius: 42, // The radius of the inner circle
-		scale: 1, // Scales overall size of the spinner
-		corners: 1, // Corner roundness (0..1)
-		color: '#fff', // #rgb or #rrggbb or array of colors
-		opacity: 0.25, // Opacity of the lines
-		rotate: 0, // The rotation offset
-		direction: 1, // 1: clockwise, -1: counterclockwise
-		speed: 1, // Rounds per second
-		trail: 60, // Afterglow percentage
-		fps: 20, // Frames per second when using setTimeout() as a fallback for CSS
-		zIndex: 2e9, // The z-index (defaults to 2000000000)
-		className: 'spinner', // The CSS class to assign to the spinner
-		top: '50vh', // Top position relative to parent
-		left: '50%', // Left position relative to parent
-		shadow: true, // Whether to render a shadow
-		hwaccel: false, // Whether to use hardware acceleration
-		position: 'fixed' // Element positioning
-	});
+			.state('app.user', {
+				url: '/user',
+				abstract: true,
+				template: '<ui-view/>'
+			})
+				.state('app.user.dashboard', {
+					url: '/dashboard',
+					data: {
+						pageTitle: 'Page.Title.Dashboard',
+						pageLead: 'Page.Brief.Dashboard'
+					},
+					views:{
+						'':{
+							templateUrl: 'views/user-panel/user-panel.html',
+							controller: 'UserPanelController',
+						},
+						'analytics@app.user.dashboard':{
+							templateUrl: 'views/analytics/analytics.html',
+							controller: 'AnalyticsController'
+						}
+					},
+					resolve: {
+						loadPlugin: ['$ocLazyLoad', function($ocLazyLoad) {
+							return $ocLazyLoad.load('./js/dummy.js');
+						}]
+					}
+				})
 
-}])
-.run(['$rootScope', '$state', 'Auth', function ($rootScope, $state, Auth){
-	$rootScope.$state = $state;
-	$rootScope.$on('$stateChangeStart', function (event, toState) {
-		if ( ( toState.data === undefined || toState.data.requireAuth === undefined || toState.data.requireAuth ) && !Auth.authenticated()) {
-			event.preventDefault();
-			$state.go('app.sign-in');
-		}
-	});
-}]);
+			.state('app.admin', {
+				url: '/admin',
+				abstract: true,
+				template: '<ui-view/>'
+			})
+				.state('app.admin.dashboard', {
+					url: '/dashboard',
+					data: {
+						pageTitle: 'Page.Title.Dashboard',
+						pageLead: 'Page.Brief.Dashboard'
+					},
+					views:{
+						'':{
+							templateUrl: 'views/admin-panel/admin-panel.html',
+							controller: 'AdminPanelController',
+						},
+						'analytics@app.admin.dashboard':{
+							templateUrl: 'views/analytics/analytics.html',
+							controller: 'AnalyticsController'
+						},
+						'diag@app.admin.dashboard':{
+							templateUrl: 'views/diag/diag.html',
+							controller: 'DiagController'
+						}
+					},
+					resolve: {
+						loadPlugin: ['$ocLazyLoad', function($ocLazyLoad) {
+							return $ocLazyLoad.load('./js/dummy.js');
+						}]
+					}
+				})
+		;
+
+		usSpinnerConfigProvider.setDefaults({
+			lines: 13, // The number of lines to draw
+			length: 28, // The length of each line
+			width: 14, // The line thickness
+			radius: 42, // The radius of the inner circle
+			scale: 1, // Scales overall size of the spinner
+			corners: 1, // Corner roundness (0..1)
+			color: '#fff', // #rgb or #rrggbb or array of colors
+			opacity: 0.25, // Opacity of the lines
+			rotate: 0, // The rotation offset
+			direction: 1, // 1: clockwise, -1: counterclockwise
+			speed: 1, // Rounds per second
+			trail: 60, // Afterglow percentage
+			fps: 20, // Frames per second when using setTimeout() as a fallback for CSS
+			zIndex: 2e9, // The z-index (defaults to 2000000000)
+			className: 'spinner', // The CSS class to assign to the spinner
+			top: '50vh', // Top position relative to parent
+			left: '50%', // Left position relative to parent
+			shadow: true, // Whether to render a shadow
+			hwaccel: false, // Whether to use hardware acceleration
+			position: 'fixed' // Element positioning
+		});
+	}
+])
+.run(['$rootScope', '$state', 'Auth',
+	function ($rootScope, $state, Auth) {
+		$rootScope.$state = $state;
+		$rootScope.$on('$stateChangeStart', function (event, toState) {
+			if ( ( toState.data === undefined || toState.data.requireAuth === undefined || toState.data.requireAuth ) && !Auth.authenticated()) {
+				event.preventDefault();
+				$state.go('app.sign-in');
+			}
+		});
+	}
+]);
---
+++ client/js/dummy.js
@@ -0,0 +1,3 @@
+(function(){
+	console.log('dummy.js loaded by $ocLazyLoad: you can remove this file, it is used as an example of lazy loading for states; see client/app.js for usage');
+})();
