c3a4b891fa28fcc151714c9cf13777416161759b
rfprod <zoidenmacht@zoho.com>
2017-04-24 02:23:38 -0700

    (update) improved chech db dir script

--- check-db-dir.sh
+++ check-db-dir.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [ ! -d ~/mongo ]; then
+	mkdir ~/mongo
+	echo 'created directory for mongo databases'
+else
+	echo 'directory for mongo database exists'
+fi
+
 if [ ! -d ~/mongo/ng1nmc ]; then
 	mkdir ~/mongo/ng1nmc
-	echo 'created directory for mongodb'
+	echo 'created directory for app database'
 else
-	echo 'directory for mongodb exists'
+	echo 'directory for app database exists'
 fi
